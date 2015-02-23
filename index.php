efee877a8664f622f4ede55041f505de0eae5233
Creeplays <Creeplays@users.noreply.github.com>
2015-02-23 20:28:15 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!doctype html>
 <html>
 <body>
-Deobfuscator: http://creepworks.herokuapp.com/deobf/index.html
+CreepWorks
+<a href="http://creepworks.herokuapp.com/deobf/index.html">JavaScript Tools</a>
 <script type="text/javascript">
   var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
   document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
