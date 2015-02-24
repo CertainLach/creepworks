1ba66abe2970665eca22647cfff731e348d6e87e
Creeplays <Creeplays@users.noreply.github.com>
2015-02-24 22:40:19 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,6 @@
 <body>
 CreepWorks
 <a href="http://creepworks.herokuapp.com/deobf/index.html">JavaScript Tools</a>
-<script type="text/javascript">
-  var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-  document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-  try {
-    var pageTracker = _gat._getTracker("UA-60020223-1");
-    pageTracker._trackPageview();
-  } 
-  catch(err) {
-    alert('GA Error: '+err);    
-  }
-</script>
+<a href="https://github.com/Creeplays/creepworks">Source code</a>
 </body>
 </html>
