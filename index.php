932bb7f0ea25f8cc5e60841f3b946c4567becae7
Creeplays <Creeplays@users.noreply.github.com>
2015-03-02 15:05:01 +0500

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ CreepWorks||<a href="http://darkworks.herokuapp.com/">DarkWorks</a>
 <br>
 <a href="http://creepworks.herokuapp.com/deobf/index.html">JavaScript Tools</a>
 <br>
+<a href="http://creepworks.herokuapp.com/cpp/index.html">C++ Tools</a>
+<br>
 <a href="https://github.com/Creeplays/creepworks">Source code</a>
 </body>
 </html>
