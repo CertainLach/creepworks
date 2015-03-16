a21bff6914490d81637c5d763c50644806c20f97
Creeplays <Creeplays@users.noreply.github.com>
2015-03-16 19:32:52 +0500

    Create header.js

---
+++ header.js
@@ -0,0 +1,3 @@
+function addheader(){
+  document.write ("CreepWorks||<a href="http://darkworks.herokuapp.com/">DarkWorks</a><br><a href="http://creepworks.herokuapp.com/">Main page</a><br><a href="https://github.com/Creeplays/creepworks">Source code</a>"); 
+}
